317b87fae30275eec7ca09b5049bf7f3b3b23c49
Sven Büchner <sven.buechner.privat@googlemail.com>
2019-09-09 12:46:45 +0200

    First commit.

---
+++ UnitTestTest/UnitTestTestTests/ProgramTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestTest.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        
+
+        [TestMethod()]
+        public void ReadASCIIFileTestPass()
+        {
+            Program program = new Program();
+            Assert.IsTrue(program.ReadASCIIFile(new Uri(@"..\Ressources\CorrectFile.txt")));
+        }
+
+        [TestMethod()]
+        public void ReadASCIIFileTestEmptyFile()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReadASCIIFileTestDoesNotExist()
+        {
+
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// letsd think about this later
+        /// </summary>
+        [TestMethod()]
+        public void ReadASCIIFileTestFormattingError1()
+        {
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// Each usecase must be on EITHER a new line or punctuation. 
+        /// </summary>
+        [TestMethod()]
+        public void ReadASCIIFileTestFormattingError2()
+        {
+            Assert.Fail();
+        }
+    }
+}
